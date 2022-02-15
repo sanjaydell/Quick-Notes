372b01db846500c27d638d62f377fbc7dab93cd6
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-15 23:57:34 +0530

    Updated Styling for Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Card.css'
 
 const Card = ({color}) => {
 return(
   <div style={{ padding: '2rem' }}>
-    <div className={`card text-white bg-${color} mb-3`} style={{ maxWidth: '19rem', height: '20rem' }}>
+    <div className={`card text-white bg-${color} mb-3`} style={{ maxWidth: '16rem', height: '20rem' }}>
       <div className='card-header'>Header</div>
       <div className='card-body'>
         <h5 className='card-title'>Primary card title</h5>
