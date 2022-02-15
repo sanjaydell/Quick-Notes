cae8d7e138bd431141a17c5869de4b4a98ed138b
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-15 23:45:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+// import * as bootstrap from 'bootstrap'
+import Card from './Card'
 
 function App() {
+  const cardColors = ['success', 'primary', 'warning', 'info', 'danger', 'dark']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Card color={cardColors[0]} />
+      <Card color={cardColors[1]} />
+      <Card color={cardColors[2]} />
+      <Card color={cardColors[3]} />
+      {/* <Card color={cardColors[4]} />
+      <Card color={cardColors[5]} /> */}
     </div>
-  );
+  )
 }
 
 export default App;
