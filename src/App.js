7282558e96838aeeb8ba56713f1bbf3df8511bc0
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-19 14:40:30 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ function App() {
   const cardColors = ['success', 'primary', 'warning', 'info', 'danger', 'dark']
   return (
     <div className='App'>
-      <Card color={cardColors[0]} />
-      <Card color={cardColors[1]} />
-      <Card color={cardColors[2]} />
-      <Card color={cardColors[3]} />
-      {/* <Card color={cardColors[4]} />
-      <Card color={cardColors[5]} /> */}
+      <div className='header'>
+        sanjay
+      </div>
+      <div className='card-grid'>
+        <Card color={cardColors[0]} />
+        <Card color={cardColors[1]} />
+        <Card color={cardColors[2]} />
+        <Card color={cardColors[3]} />
+        <Card color={cardColors[4]} />
+        <Card color={cardColors[5]} />
+        <Card />
+      </div>
     </div>
   )
 }
