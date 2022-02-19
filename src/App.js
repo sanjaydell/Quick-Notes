8771e720ca49725fe7c8feaf0380df5b457d6d00
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-19 17:53:07 +0530

    Connected Firebase Database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './app.css'
+import { db } from './firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 // import * as bootstrap from 'bootstrap'
 import Card from './Card'
 import NewCard from './NewCard'
+import EditCard from './EditCard'
 
 function App() {
+  const [notes, setNotes] = useState()
   const cardColors = ['success', 'primary', 'warning', 'info', 'danger', 'dark']
   const [addNewNote, setAddNewNote] = useState(false)
+  const notesCollectionRef = collection(db, '1')
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const data = await getDocs(notesCollectionRef)
+      setNotes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
+    }
+
+    getNotes()
+  }, [])
   return (
     <div className='App'>
       <div className='header'>
         sanjay
       </div>
       <div className='card-grid'>
+        {notes && notes.map((data) => {
+          return (<Card color={cardColors[data.color]} title={data.title} time='' note={data.note}/>)
+        })}
         {addNewNote && (
           <>
-            <Card color={cardColors[0]} />
-            <Card color={cardColors[1]} />
-            <Card color={cardColors[2]} />
-            <Card color={cardColors[3]} />
-            <Card color={cardColors[4]} />
-            <Card color={cardColors[5]} />
+            <EditCard color={cardColors[Math.floor(Math.random() * 6)]} />
           </>
           )
         }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './card.css'
 
-const Card = ({color}) => {
+const Card = ({color, title, time, note }) => {
 return(
   <div style={{ padding: '2rem' }}>
     <div className={`card text-white bg-${color} mb-3`} style={{ width: '16rem', height: '20rem' }}>
-      <div className='card-header'>Header</div>
+      <div className='card-header'>{title}</div>
       <div className='card-body'>
-        <h5 className='card-title'>Primary card title</h5>
-        <textarea className='card-text-area' placeholder='Enter a Note'/>
-        {/* <input type='text' placeholder='' /> */}
-        {/* <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+        <h5 className='card-title'>{time}</h5>
+        <p className='card-text'>{note}</p>
       </div>
     </div>
   </div>
