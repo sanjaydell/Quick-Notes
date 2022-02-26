6b1c36889f34dda4704aec4e8095cd21cbfd3a07
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-26 16:00:42 +0530

    Added Validations for Login and Signup

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,27 +1,64 @@
 import React from 'react'
 import './login.css'
+import { useForm } from 'react-hook-form'
 
 function Login({onEmailChange, onPasswordChange, onLoginClick, onRegisterClick, onGuestLoginClick}) {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onSubmit = () => {
+    onLoginClick()
+  }
+
   return (
     <>
       <div className='login'>
         <div className='login-image'>
           <img src='QuickNote.png' alt='' />
         </div>
-        <div className='login-fields'>
-          <input type='text' placeholder='  Enter Email-Id' onChange={(event) => onEmailChange(event.target.value)} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='login-fields'>
+            <input
+              {...register('email',
+              {pattern: {
+                value: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/,
+                message: 'Enter valid email'
+              },required : {
+                value: true,
+                message: 'Enter a email'
+              }}
+              )}
+              type='text'
+              placeholder='  Enter Email-Id'
+              onChange={(event) => onEmailChange(event.target.value)}
+            />
+            {errors.email && <span style={{ color: 'red' }}>{errors.email.message}</span>}
+            <br />
+            <input
+              {...register('password',
+              {required: {
+                value: true,
+                message: 'Enter Password'
+              },minLength : {
+                value: 6,
+                message: 'Minimum 6 characters required'
+              }}
+              )}
+              type='password'
+              placeholder='  Enter Password'
+              onChange={(event) => onPasswordChange(event.target.value)}
+            />
+            {errors.password && <span style={{ color: 'red' }}>{errors.password.message}</span>}
+          </div>
+          <div className='primary' style={{ paddingLeft: '26px' }}>
+            <button type='submit'> Log-In </button>
+          </div>
+        </form>
+        <div className='login-buttons' style={{ paddingLeft: '18px' }}>
+          <button style={{ marginLeft: '55px' }} onClick={() => onGuestLoginClick()}>Guest Login</button>
           <br />
-          <br />
-          <input type='password' placeholder='  Enter Password' onChange={(event) => onPasswordChange(event.target.value)} />
-        </div>
-        <div className='primary'>
-          <button onClick={() => onLoginClick() }> Log-In </button>
-        </div>
-        <div className='login-buttons'>
-          <br />
-          <button style={{ marginRight: '25px' }} onClick={() => onRegisterClick(false) }> Register </button>
-          <br />
-          <button style={{ marginLeft: '5px' }} onClick={() => onGuestLoginClick()}>Guest Login</button> 
+          <h6>
+            Don't have an account?
+            <a href='javascript:;' style={{ marginRight: '25px' }} onClick={() => onRegisterClick(false) }> Register </a>
+          </h6>
         </div>
       </div>
     </>
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,28 +1,76 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function SignUp({onEmailChange, onPasswordChange, onRegisterClick, onLoginClick, onGuestLoginClick, onNameChange}) {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onRegister = () => {
+    onRegisterClick()
+  }
+
   return (
     <div className='login'>
       <div className='login-image'>
-          <img src='QuickNote.png' alt='' />
+        <img src='QuickNote.png' alt='' />
       </div>
-      <div className='login-fields'>
-        <input type='text' placeholder='  Enter Email-Id' onChange={(event) => onEmailChange(event.target.value)} />
-        <br />
-        <br />
-        <input type='password' placeholder='  Enter Password' onChange={(event) => onPasswordChange(event.target.value)} />
-        <br />
-        <br />
-        <input type='text' placeholder='  Enter your Name' onChange={(event) => onNameChange(event.target.value)} />
-      </div>
-      <div className='primary'>
-      <button onClick={() => onRegisterClick() }> Register </button>
-      </div>
+      <form onSubmit={handleSubmit(onRegister)}>
+        <div className='login-fields'>
+          <input
+            {...register('email',
+            {pattern: {
+              value: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/,
+              message: 'Enter valid email'
+            },required : {
+              value: true,
+              message: 'Enter a email'
+            }}
+            )}
+            type='text'
+            placeholder='  Enter Email-Id'
+            onChange={(event) => onEmailChange(event.target.value)}
+          />
+          {errors.email && <span style={{ color: 'red' }}>{errors.email.message}</span>}
+          <br />
+          <input
+            {...register('password',
+            {required: {
+              value: true,
+              message: 'Enter Password'
+            },minLength : {
+              value: 6,
+              message: 'Minimum 6 characters required'
+            }}
+            )}
+            type='password'
+            placeholder='  Enter Password'
+            onChange={(event) => onPasswordChange(event.target.value)}
+          />
+          {errors.password && <span style={{ color: 'red' }}>{errors.password.message}</span>}
+          <br />
+          <input
+            {...register('name',
+            {required: {
+              value: true,
+              message: 'Enter a name'
+            },maxLength : {
+              value: 12,
+              message: 'Maximum 6 characters'
+            }}
+            )}
+            type='text'
+            placeholder='  Enter your Name'
+            onChange={(event) => onNameChange(event.target.value)}
+          />
+          {errors.name && <span style={{ color: 'red' }}>{errors.name.message}</span>}
+        </div>
+        <div className='primary' style={{ paddingLeft: '26px' }}>
+          <button type='submit' style={{ marginRight: '10px' }} > Register </button>
+        </div>
+      </form>
       <div className='login-buttons'>
-        <br />
-        <button style={{ marginRight: '25px' }} onClick={() => onLoginClick(true) }> Log-In </button>
-        <br />
-        <button style={{ marginLeft: '5px' }} onClick={() => onGuestLoginClick()}>Guest Login</button> 
+        <h6>
+          Already have an account?
+          <a href='javascript:;' style={{ marginRight: '10px' }} onClick={() => onLoginClick(true) }> Login </a>
+        </h6>
       </div>
     </div>
   )
