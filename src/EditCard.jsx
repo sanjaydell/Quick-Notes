6ad8affa88b8487963a41ae7e691ddc5db0264e9
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-02-26 10:28:23 +0530

    Added Validation for New Card

--- src/EditCard.jsx
+++ src/EditCard.jsx
@@ -1,23 +1,60 @@
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete'
 import './edit-card.css'
+import { useForm } from 'react-hook-form'
 
 function EditCard({color, onTitleChange, onNoteChange, onClick, onDelete}) {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+  const onAdd = () => {
+    onClick()
+  }
   return (
     <div style={{ padding: '2rem' }}>
-    <div className={`card text-white bg-${color} mb-3`}>
-      <div className='edit-card-header'>
-        <input type='text' placeholder='Add Title' onChange={(event) => onTitleChange(event.target.value) }/>
-        <div className='delete-edit-card'>
-          <DeleteIcon onClick={() => onDelete(false)} />
+      <div className={`card text-white bg-${color} mb-3`} style={{ paddingBottom: '0.5rem' }}>
+        <form onSubmit={handleSubmit(onAdd)}>
+        <div className='edit-card-header'>
+          <input
+            name='title'
+            {...register('title',
+              {required: {
+                value: true,
+                message: 'Enter a Title'
+              },maxLength : {
+                value: 20,
+                message: 'Max-Limit 20 characters'
+              }}
+            )}
+            type='text'
+            placeholder='Add Title'
+            onChange={(event) => onTitleChange(event.target.value) }
+          />
+          <div className='delete-edit-card'>
+            <DeleteIcon onClick={() => onDelete(false)} />
+          </div>
         </div>
-      </div>
-      <div className='edit-card-body'>
-        <textarea className='card-text-area' placeholder='Enter a Note' onChange={(event) => onNoteChange(event.target.value)}/>
-        <button onClick={() => onClick()}> ADD </button>
+        {errors.title && <span style={{ color: 'red' }}>{errors.title.message}</span>}
+        <div className='edit-card-body'>
+          <textarea
+            {...register('text',
+              {required: {
+                value: true,
+                message: 'Enter a note'
+              },maxLength : {
+                value: 100,
+                message: 'Max-Limit 100 characters'
+              }}
+            )}
+            className='card-text-area'
+            placeholder='Enter a Note'
+            onChange={(event) => onNoteChange(event.target.value)}
+          />
+          {errors.text && <span style={{ color: 'red' }}>{errors.text.message}</span>}
+          <br />
+          <button type='submit'> ADD </button>
+        </div>
+        </form>
       </div>
     </div>
-  </div>
   )
 }
 
